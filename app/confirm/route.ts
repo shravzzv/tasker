c54a0460081bd6c6b3e5c66906196f9ec7a68d27
Sai Shravan <saishravan384@gmail.com>
2025-10-06 15:31:40 +0530

    feat: redirect the user to /dashboard on confirm link click in email

--- app/confirm/route.ts
+++ app/confirm/route.ts
@@ -7,7 +7,6 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const token_hash = searchParams.get('token_hash')
   const type = searchParams.get('type') as EmailOtpType | null
-  const next = searchParams.get('next') ?? '/'
 
   if (token_hash && type) {
     const supabase = await createClient()
@@ -16,12 +15,13 @@ export async function GET(request: NextRequest) {
       type,
       token_hash,
     })
+
     if (!error) {
       // redirect user to specified redirect URL or root of app
-      redirect(next)
+      redirect('/dashboard')
     }
   }
 
   // redirect the user to an error page with some instructions
-  redirect('/error')
+  redirect('/signup-error')
 }
