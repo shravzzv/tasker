3b0d2063ffe07a9163c75ed3ca1118c733d3fe33
Sai Shravan <saishravan384@gmail.com>
2025-10-10 14:43:54 +0530

    feat: add sonner toasts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
+import { Toaster } from 'sonner'
 
 const inter = Inter({
   variable: '--font-inter',
@@ -23,6 +24,7 @@ export default function RootLayout({
     <html lang='en'>
       <body className={`${inter.variable} antialiased`}>
         {children}
+        <Toaster richColors />
         <Analytics />
       </body>
     </html>
