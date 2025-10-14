7085880783bbd1532de9053df505f7febec7fcee
Sai Shravan <saishravan384@gmail.com>
2025-10-14 15:51:27 +0530

    feat: add GitHub authentication

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -19,6 +19,7 @@ import { Input } from '@/components/ui/input'
 import { Spinner } from '@/components/ui/spinner'
 import Link from 'next/link'
 import GoogleAuthButton from '@/components/google-auth-button'
+import GithubAuthButton from '@/components/github-auth-button'
 
 const formSchema = z.object({
   email: z.email('Please enter a valid email'),
@@ -148,7 +149,10 @@ export default function SignInPage() {
         </div>
       </div>
 
-      <GoogleAuthButton />
+      <div className='flex flex-col gap-4'>
+        <GoogleAuthButton />
+        <GithubAuthButton />
+      </div>
 
       <p className='text-center text-sm text-muted-foreground mt-6'>
         Don&apos;t have an account?{' '}
